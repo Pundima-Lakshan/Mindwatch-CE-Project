16a76eb00cc29d044b6b2d6cb296a61774330c93
rvndudz <63877248+rvndudz@users.noreply.github.com>
2023-10-22 21:32:43 +0530

    Implementation of class structure #2
    Removed annotate_frame() and process_webcam()

--- violence.py
+++ violence.py
@@ -3,82 +3,79 @@ import torch
 import cv2
 from transformers import CLIPProcessor, CLIPModel
 import queue
+import csv  # Import the csv module
 
+class Aggressive_behavior_detection:
+    def __init__(self, input_path, output_csv_path, frames_to_analyze, model):
+        self.input_path = input_path
+        self.output_csv_path = output_csv_path
+        self.frames_to_analyze = frames_to_analyze
+        self.model = model
 
-class VideoAnalyzer:
-    def __init__(self):
-        self.FRAMES_TO_ANALYZE = 5
         self.output_queue = queue.Queue()
 
         if torch.cuda.is_available():
-            self.device = torch.device("cuda")
             print("GPU is available")
         else:
-            self.device = torch.device("cpu")
             print("GPU is not available")
 
-        self.clip_processor = CLIPProcessor.from_pretrained(
-            "openai/clip-vit-base-patch32")
-        self.clip_model = CLIPModel.from_pretrained(
-            "openai/clip-vit-base-patch32")
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+        self.clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        if (model == 16):
+            self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
+        else:
+            self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        
         self.clip_model = self.clip_model.to(self.device)
 
     def get_probabilities_for_frame(self, image, labels=['violent scene', 'non-violent scene']):
-        inputs = self.clip_processor(
-            text=labels, images=image, return_tensors="pt", padding=True).to(self.device)
+        inputs = self.clip_processor(text=labels, images=image, return_tensors="pt", padding=True).to(self.device)
         outputs = self.clip_model(**inputs)
-
         logits_per_image = outputs.logits_per_image
         probs = logits_per_image.softmax(dim=1)
-        probs_dict = {labels[i]: probs[0][i].item()
-                      for i in range(len(labels))}
+        probs_dict = {labels[i]: probs[0][i].item() for i in range(len(labels))}
         return probs_dict
 
-    def annotate_frame(self, frame, probability_threshold):
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        bottom_left_corner = (10, 30)
-        font_scale = 1
-        font_color = (0, 0, 255)
-        font_thickness = 2
-        text = f"Violent Probability: {probability_threshold:.2f}"
-        cv2.putText(frame, text, bottom_left_corner, font,
-                    font_scale, font_color, font_thickness)
-
-    def process_video(self, input_path):
-        vs = cv2.VideoCapture(input_path)
-
+    def process_video(self):
+        vs = cv2.VideoCapture(self.input_path)
         last_scores = []
         frame_count = 0
 
-        while True:
-            (grabbed, frame) = vs.read()
+        with open(self.output_csv_path, mode='w', newline='') as csv_file:
+            fieldnames = ['Frame Number', 'Violence Probability']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
 
-            if not grabbed:
-                break
+            while True:
+                (grabbed, frame) = vs.read()
 
-            try:
-                image = Image.fromarray(frame)
-                image = image.convert("RGB")
-                probs = self.get_probabilities_for_frame(image)
-                violent_probability = probs["violent scene"]
-            except Exception as e:
-                violent_probability = 0
+                if not grabbed:
+                    break
 
-            last_scores.append(violent_probability)
-            if len(last_scores) > self.FRAMES_TO_ANALYZE:
-                last_scores = last_scores[1:]
+                try:
+                    image = Image.fromarray(frame)
+                    image = image.convert("RGB")
+                    probs = self.get_probabilities_for_frame(image)
+                    violent_probability = probs["violent scene"]
+                except Exception as e:
+                    violent_probability = 0
 
-            final_score = max(last_scores)
-            self.output_queue.put((frame_count, final_score))
+                last_scores.append(violent_probability)
+                if len(last_scores) > self.frames_to_analyze:
+                    last_scores = last_scores[1:]
 
-            print(f"Frame {frame_count}: Violent Probability - {final_score}")
+                final_score = max(last_scores)
+                self.output_queue.put((frame_count, final_score))
 
-            frame_count += 1
+                frame_count += 1
 
-            key = cv2.waitKey(1) & 0xFF
+                key = cv2.waitKey(1) & 0xFF
 
-            if key == ord("q"):
-                break
+                if key == ord("q"):
+                    break
+
+                writer.writerow({'Frame Number': frame_count, 'Violence Probability': final_score})
 
         vs.release()
         cv2.destroyAllWindows()
@@ -87,51 +84,10 @@ class VideoAnalyzer:
             frame_num, score = self.output_queue.get()
             print(f"Frame {frame_num}: Violent Probability - {score}")
 
-        print("[INFO] Video processing complete.")
-
-    def process_webcam(self):
-        vs = cv2.VideoCapture(0)
-
-        while True:
-            (grabbed, frame) = vs.read()
-
-            if not grabbed:
-                break
-
-            try:
-                image = Image.fromarray(frame)
-                image = image.convert("RGB")
-                probs = self.get_probabilities_for_frame(image)
-                violent_probability = probs["violent scene"]
-            except Exception as e:
-                violent_probability = 0
-
-            self.annotate_frame(frame, violent_probability)
-            cv2.imshow("Webcam Feed", frame)
-
-            key = cv2.waitKey(1) & 0xFF
-
-            if key == ord("q"):
-                break
-
-        vs.release()
-        cv2.destroyAllWindows()
-
-    def main(self):
-        print("Choose an option:")
-        print("1. Investigate a video file")
-        print("2. Examine live webcam input")
-        option = input("Enter the option (1/2): ")
-
-        if option == "1":
-            input_video_path = '4.mp4'
-            self.process_video(input_video_path)
-        elif option == "2":
-            self.process_webcam()
-        else:
-            print("Invalid option. Please choose 1 or 2.")
-
-
 if __name__ == "__main__":
-    video_analyzer = VideoAnalyzer()
-    video_analyzer.main()
+    input_video_path = '1.mp4'
+    output_csv_path = 'output.csv'
+    frames_to_analyze = 5
+    model = 16
+    processor = Aggressive_behavior_detection(input_video_path, output_csv_path, frames_to_analyze, model)
+    processor.process_video()
