77b93b594a364e2d99136a78b84154606a534faa
Pundima Lakshan <pundimal@gmail.com>
2023-11-21 23:04:02 +0530

    add standing on bed

---
+++ Functions/standing_on_bed.py
@@ -0,0 +1,11 @@
+from vilt import ask_questions_video
+
+# VIDEO_PATH = "D:\\Projects\\1 CEProject\\resources\\videos\\2.mp4"
+QUESTION_TEXTS = "Is the person standing on a bed?"
+
+def analyze_standing_on_bed(VIDEO_PATH):
+    ask_questions_video(VIDEO_PATH, QUESTION_TEXTS, "standing_on_bed")
+
+if __name__ == "__main__":
+    VIDEO_PATH = "D:\\Projects\\1 CEProject\\resources\\videos\\2.mp4"
+    analyze_standing_on_bed(VIDEO_PATH)
